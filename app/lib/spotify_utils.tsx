2afa562db2835b922445accb11ac80bf4eeedfce
Adamita David Stefan <adamita.david@gmail.com>
2026-08-29 00:40:22 +0300

    Added refresh spotify token function

--- app/lib/spotify_utils.tsx
+++ app/lib/spotify_utils.tsx
@@ -1,5 +1,4 @@
 
-
 const spotifyId: string = process.env.SPOTIFY_CLIENT_ID!;
 const spotifySecret: string = process.env.SPOTIFY_CLIENT_SECRET!;
 
@@ -7,24 +6,29 @@ if(!spotifyId || !spotifySecret)
     throw new Error('Missing Spotify credentials');
 
 
-export async function getAccessToken(){
+export async function getAccessToken(): Promise<string>{
 
     const res = await fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-        body: JSON.stringify({
+        body: new URLSearchParams({
             grant_type: 'client_credentials',
             client_id: spotifyId,
             client_secret: spotifySecret
         })
     })
 
-    if(!res.ok)
+    if(!res.ok){
+        const err = await res.text();
+        console.error('Spotify API Error: ', err);
         throw new Error('Error fetching Spotify access token');
+    }
 
     const data = await res.json();
     if(!data)
         throw new Error('Error fetching Spotify access token');
 
-    return data;
+    return data.access_token;
 }
+
+
