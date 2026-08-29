dc4cbcc8ab1eab0479f0cd7864f0197fc74472c3
Adamita David Stefan <adamita.david@gmail.com>
2026-08-29 15:16:18 +0300

    Function to get Deezer preview link and song details

---
+++ app/api/cron/refresh-pool/route.tsx
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const genres = ['pop', 'rock', '']
+
+export async function GET(req: NextRequest){
+
+
+}
---
+++ lib/deezer_utils.tsx
@@ -0,0 +1,23 @@
+
+const DEEZER_BASE_URL = 'https://api.deezer.com'
+
+export async function getPreviewLink(artistName: string, songName: string){
+
+    const query = `artist:"${artistName}" track:"${songName}"`;
+    const res = await fetch(`${DEEZER_BASE_URL}/search?q=${encodeURIComponent(query)}`);
+
+    if(!res.ok){
+        const err = await res.text();
+        console.error(err);
+        throw new Error('Error fetching Deezer response');
+    }
+
+    const data = await res.json();
+    if(!data){
+        throw new Error('No data returned by Deezer');
+    }
+
+    const songData = data.data[0];
+    return {title: songData.title, artist: songData.artist.name, previewLink: songData.preview};
+    
+}
