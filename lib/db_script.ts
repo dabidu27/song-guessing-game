6a07108848bdc865d750b307a6009f4ded60efe0
Adamita David Stefan <adamita.david@gmail.com>
2026-09-02 18:23:08 +0300

    Spotify utils for gettings songs out of my personal playlists

--- lib/db_script.ts
+++ lib/db_script.ts
@@ -1,6 +1,7 @@
 import {Pool} from 'pg';
 import { parseCsv } from './dataset_utils';
 import { getTrackId } from './deezer_utils';
+import { getSongsFromPlaylists} from './spotify_utils';
 import { songsArray } from './songs_array';
 
 const pool = new Pool({connectionString: process.env.DATABASE_URL});
@@ -14,17 +15,20 @@ interface Song{
 async function fillDb(){
 
     //const songs: Song[] = parseCsv();
+    //const songs: Song[] = await getSongsFromPlaylists();
     const songs: Song[] = songsArray;
-
+    
     for(const song of songs){
         try{
             
             //No preview link => throws error => song is skipped
             const deezerData = await getTrackId(song.mainArtist, song.trackName);
 
-            const result = await pool.query('insert into songs_spotify(title, artist, year, track_id) values($1, $2, $3, $4)', 
+            const result = await pool.query(`insert into songs_spotify(title, artist, year, track_id) values($1, $2, $3, $4)
+                on conflict (track_id)
+                do update set title = EXCLUDED.title, artist = EXCLUDED.artist, year = EXCLUDED.year`, 
                 [song.trackName, song.mainArtist, 2026, deezerData.trackId]
-            );
+            ); //upsert - for songs that were in multiple playlists
 
             if(result.rowCount === 0){
                 console.error('Failed to insert song ', song.trackName)
---
+++ lib/spotify_utils.ts
@@ -0,0 +1,64 @@
+
+
+export async function getSongsFromPlaylists(){
+
+    const accessToken = process.env.SPOTIFY_USER_TOKEN!;
+    const songs: any[] = [];
+
+    const playlistIdsRes = await fetch('https://api.spotify.com/v1/me/playlists',
+        {headers: {"Authorization": `Bearer ${accessToken}`}}
+    )
+
+    const skipThesePlaylists = [
+        'METALCORE 2026', 'NU METAL EDGE', 'Punk-O-Rama fav', 'Playlist OpelAstra', 
+        'la multi ani raluca', 'cool girl fall', 'makeout sesh ', 'mid 90s', 
+        'My Playlist #45', 'city pop ', 'zara??!?!?!?', 'an actually accurate eddie munson playlist', 
+        'Rather be dead ', 'coming of age', 'Indigo Night', 'space out make out', 
+        'Creep', 'humid summer nights by a pretty body of water', 'radiohead.'
+    ];
+
+    let playlistIdsData = await playlistIdsRes.json();
+   
+    playlistIdsData = playlistIdsData.items.filter((pd: any) => !skipThesePlaylists.includes(pd.name));
+    const playlistIds = playlistIdsData.map((pd: any) => pd.id);
+    console.log(playlistIds);
+    console.log(`Found ${playlistIds.length} playlists to process...`);
+
+    const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+    for(const playlistId of playlistIds){
+
+        const playlistsRes = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/items`, 
+         {headers: {"Authorization": `Bearer ${accessToken}`}}
+        )
+
+        if(playlistsRes.status === 429){
+            let retryAfter = playlistsRes.headers.get('retry-after');
+            console.error(`Rate limit hit. Retry-after: ${retryAfter}. Aborting`);
+            throw new Error(`Rate limit hit. Retry-after: ${retryAfter}. Aborting`);
+        }
+
+        const playlistsData = await playlistsRes.json();
+        
+        //safeguard - if spotify returns an error (like rate limit) log and skip to the next playlist
+        if (playlistsData.error) {
+            console.error(`Error fetching playlist ${playlistId}:`, playlistsData.error.message);
+            continue; 
+        }
+
+        const songsData = playlistsData.items
+            .map((pd: any) => pd.item)
+            .filter((item: any) => item !== null);
+
+        songsData.forEach((song: any) => {
+            if (song && song.name && song.artists) {
+                songs.push({title: song.name, mainArtist: song.artists[0].name})
+            }
+        });
+        
+        //pause for 2 seconds to avoid rate-limiting
+        await delay(2000); 
+    }
+
+    console.log(`Successfully extracted ${songs.length} songs!`);
+    return songs;
+}
